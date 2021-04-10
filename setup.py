2ef5d7563f989ce0f5f0157ed902fc3db933c532
Ciuci <ciuci@mizar.ai>
2021-04-10 16:23:52 +0000

    release 0.1.6

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages
 from setuptools import setup
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 
 with open("requirements.txt") as f:
     requirements = f.readlines()
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("README.md", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
@@ -24,5 +24,5 @@ setup(
     project_urls={
         "Bug Tracker": "https://github.com/MizarAI/mizar-labs/issues",
     },
-    python_requires=">=3.8"
+    python_requires=">=3.8",
 )
