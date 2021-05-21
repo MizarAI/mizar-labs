f63774b5ab81bb902268f30a61f72a4c2eea722f
alexw <alexwong_92@hotmail.com>
2021-05-21 18:04:06 +0000

    RELEASE: 0.1.121

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-__version__ = "0.1.12"
+__version__ = "0.1.121"
 
 with open("requirements.txt") as f:
     requirements = f.readlines()
